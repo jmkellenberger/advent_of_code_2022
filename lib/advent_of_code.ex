87dc5a3172a8407ec4b7e4ea58a616c65e89c28a
Jack Kellenberger <jackson@kellenberger.io>
2022-12-01 09:39:59 -0500

    Adds solution to D1P2

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -3,7 +3,7 @@ defmodule AdventOfCode do
   Read in a list of calories carried by elves, seperated by blank lines.
   Find number of calories carried by the elf with the most snacks.
   """
-  def day_one_part_one do
+  def day_one do
     File.read!("assets/day_one.txt")
     |> String.split(~r/\n/)
     |> Enum.chunk_by(&(&1 == ""))
@@ -11,6 +11,15 @@ defmodule AdventOfCode do
     |> Enum.map(
       &Enum.reduce(&1, 0, fn n, acc -> String.to_integer(n) + acc end)
     )
-    |> Enum.max()
+    |> Enum.sort(:desc)
+  end
+
+  def day_one_part_one do
+    hd(day_one())
+  end
+
+  def day_one_part_two do
+    [first, second, third | _] = day_one()
+    first + second + third
   end
 end
