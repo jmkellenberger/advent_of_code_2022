276f2781d4bbc6d534fb253a327c3d10d4cf5576
Jack Kellenberger <jackson@kellenberger.io>
2022-12-02 16:45:00 -0500

    Implement day two

---
+++ lib/advent_of_code/day_one.ex
@@ -0,0 +1,25 @@
+defmodule AdventOfCode.DayOne do
+  @doc """
+  Read in a list of calories carried by elves, seperated by blank lines.
+  Find number of calories carried by the elf with the most snacks.
+  """
+  def day_one do
+    File.read!("assets/day_one.txt")
+    |> String.split(~r/\n/)
+    |> Enum.chunk_by(&(&1 == ""))
+    |> Enum.reject(&(&1 == [""]))
+    |> Enum.map(
+      &Enum.reduce(&1, 0, fn n, acc -> String.to_integer(n) + acc end)
+    )
+    |> Enum.sort(:desc)
+  end
+
+  def day_one_part_one do
+    hd(day_one())
+  end
+
+  def day_one_part_two do
+    [first, second, third | _] = day_one()
+    first + second + third
+  end
+end
---
+++ lib/advent_of_code/day_two.ex
@@ -0,0 +1,33 @@
+defmodule AdventOfCode.DayTwo do
+  @input File.read!("assets/day_two.txt")
+         |> String.split(~r/\n/, trim: true)
+
+  def part_one,
+    do: Enum.reduce(@input, 0, fn match, acc -> score_match(match) + acc end)
+
+  def score_match("A X"), do: 4
+  def score_match("A Y"), do: 8
+  def score_match("A Z"), do: 3
+  def score_match("B X"), do: 1
+  def score_match("B Y"), do: 5
+  def score_match("B Z"), do: 9
+  def score_match("C X"), do: 7
+  def score_match("C Y"), do: 2
+  def score_match("C Z"), do: 6
+
+  def part_two,
+    do: Enum.reduce(@input, 0, fn match, acc -> fix_match(match) + acc end)
+
+  # X means you need to lose,
+  # Y means you need to end the round in a draw, and
+  # Z means you need to win. Good luck!"
+  def fix_match("A X"), do: 3
+  def fix_match("A Y"), do: 4
+  def fix_match("A Z"), do: 8
+  def fix_match("B X"), do: 1
+  def fix_match("B Y"), do: 5
+  def fix_match("B Z"), do: 9
+  def fix_match("C X"), do: 2
+  def fix_match("C Y"), do: 6
+  def fix_match("C Z"), do: 7
+end
