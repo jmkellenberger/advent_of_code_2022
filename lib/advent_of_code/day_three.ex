5e34e0e37f4c4a64751300b620a0340e2a53b066
Jack Kellenberger <jackson@kellenberger.io>
2022-12-03 01:08:26 -0500

    Adds solution to day 3

---
+++ lib/advent_of_code/day_three.ex
@@ -0,0 +1,52 @@
+defmodule AdventOfCode.DayThree do
+  @input File.read!("assets/day_three.txt")
+         |> String.split(~r/\n/, trim: true)
+
+  @item_priority (Enum.map(?a..?z, fn x -> <<x::utf8>> end) ++
+                    Enum.map(?A..?Z, fn x -> <<x::utf8>> end))
+                 |> Enum.zip(1..52)
+                 |> Enum.into(%{})
+
+  def part_one,
+    do:
+      Enum.reduce(@input, 0, fn rucksack, acc ->
+        find_item_common_to_both_compartments(rucksack) + acc
+      end)
+
+  def part_two do
+    @input
+    |> Enum.chunk_every(3)
+    |> Enum.reduce(0, fn group, acc -> find_badge_value(group) + acc end)
+  end
+
+  defp find_item_common_to_both_compartments(rucksack) do
+    rucksack
+    |> split_rucksack()
+    |> get_common_items()
+    |> get_item_priority()
+  end
+
+  defp find_badge_value(group) do
+    group
+    |> get_common_items()
+    |> get_item_priority()
+  end
+
+  defp split_rucksack(rucksack) do
+    midpoint = (String.length(rucksack) / 2) |> trunc()
+    String.split_at(rucksack, midpoint) |> Tuple.to_list()
+  end
+
+  defp get_common_items(group) do
+    group
+    |> Enum.map(
+      &(String.split(&1, "", trim: true)
+        |> MapSet.new())
+    )
+    |> Enum.reduce(&MapSet.intersection/2)
+    |> MapSet.to_list()
+    |> Enum.join()
+  end
+
+  defp get_item_priority(item), do: @item_priority[item]
+end
